1f235db895a62721367e1fb5b6aaa450b85dd3b9
Sherzod Karimov <karimovj@gmail.com>
2018-07-20 09:17:53 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,37 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string) 
+  array.unshift(string)
+end 
+
+def using_pop(array)
+  array.pop 
+end 
+
+def pop_with_args(array)
+  array.pop(2)
+end 
+
+def using_shift(array)
+  array.shift 
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end 
+
+def using_concat(arr1, arr2)
+  arr1.concat(arr2)
+end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
